c002f22a148c328a82009208c8de2dba05909a55
Maximiliano Carrera Oropeza <133275896+MaximilianoCarreraOropeza@users.noreply.github.com>
2025-11-19 10:49:45 -0600

    Sistema de empujar y jalar de objetos con el personaje (#9)
    * [ADD] agrego mas parte del nivel 1 y corrigo los assests de los controlles
    
    * [ADD] se agrega sistema de interaccion con los objetos

--- Assets/Scripts/KnightController.cs
+++ Assets/Scripts/KnightController.cs
@@ -3,17 +3,24 @@ using UnityEngine;
 
 public class KnightController : MonoBehaviour
 {
+    [Header("Configuracion de Componentes")]
     public Animator animator;
     public SpriteRenderer spriteRenderer;
+    public Collider2D standingCollider;
+    public Collider2D rollingCollider; 
+    [Header("Parametros de Movimiento")]
     public float runningSpeed = 5f;
     public float jumpForce = 5f;
+    [Header("Layer de Suelo")]
     public LayerMask groundLayer;
+    [HideInInspector] public bool isGrabbingBox = false;
+
     
     public static KnightController Instance;
     
     private Rigidbody2D _rigidbody2D;
-    private float _horizontalInput;
-    private float _verticalInput;
+    private bool _isRolling = false;
+
     
     void Awake()
     {  
@@ -23,62 +30,107 @@ public class KnightController : MonoBehaviour
 
     void Update()
     {
-        _horizontalInput = Input.GetAxisRaw("Horizontal");
-        _verticalInput = Input.GetAxisRaw("Vertical");
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Jump();
-        }
-    }
+        // Inputs
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
 
-    void FixedUpdate()
-    {
-        float horizontalInput = _horizontalInput;
-        
-        float verticalInput = _verticalInput;
-        
-        if (!(horizontalInput == 0))
+        // 1. DETECCIÓN DE INTENCIÓN DE RODAR
+        if (verticalInput < 0 && IsTouchingTheGround())
         {
-            spriteRenderer.flipX = horizontalInput < 0f;
-            
-            var targetVelocityX = horizontalInput * runningSpeed;
-
-            _rigidbody2D.linearVelocity = new Vector2(targetVelocityX, _rigidbody2D.linearVelocity.y);
-                
-            animator.SetBool("isRunning", true);
-            
-        }else
-        {
-            animator.SetBool("isRunning", false);
-        }
-
-        if (verticalInput < 0)
-        {
-            animator.SetBool("isRolling", true);
+            if (!_isRolling) StartRoll(); // Si no estaba rodando, empieza
         }
         else
         {
-            animator.SetBool("isRolling", false);
+            if (_isRolling) EndRoll(); // Si suelta, termina
         }
 
+        // 2. MOVIMIENTO Y SALTO
+        if (_isRolling)
+        {
+            // Comportamiento al rodar
+            HandleRollingMovement(horizontalInput);
+        }
+        else
+        {
+            // Comportamiento normal
+            HandleNormalMovement(horizontalInput);
+            if (Input.GetKeyDown(KeyCode.Space) && !isGrabbingBox) Jump();
+        }
+    }
+
+
+    void StartRoll()
+    {
+        _isRolling = true;
+        animator.SetBool("isRolling", true);
+        standingCollider.enabled = false;
+        rollingCollider.enabled = true;
+    }
+
+    void EndRoll()
+    {
+        _isRolling = false;
+        animator.SetBool("isRolling", false);
+        rollingCollider.enabled = false;
+        standingCollider.enabled = true;
+    }
+
+    void HandleNormalMovement(float xInput)
+    {
+        if (xInput != 0)
+        {
+            spriteRenderer.flipX = xInput < 0;
+            _rigidbody2D.linearVelocity = new Vector2(xInput * runningSpeed, _rigidbody2D.linearVelocity.y);
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            _rigidbody2D.linearVelocity = new Vector2(0, _rigidbody2D.linearVelocity.y);
+            animator.SetBool("isRunning", false);
+        }
+    }
+
+    void HandleRollingMovement(float xInput)
+    {
+        if (xInput != 0)
+        {
+             spriteRenderer.flipX = xInput < 0;
+            // Misma velocidad o reducida
+            _rigidbody2D.linearVelocity = new Vector2(xInput * runningSpeed, _rigidbody2D.linearVelocity.y);
+        }
+        else
+        {
+             // Si no se mueve, se queda quieto agachado
+             _rigidbody2D.linearVelocity = new Vector2(0, _rigidbody2D.linearVelocity.y);
+        }
     }
 
     void Jump()
     {
-        if (!IsTouchingTheGround()) return;
-        _rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        if (IsTouchingTheGround())
+        {
+            _rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
+
     
     bool IsTouchingTheGround()
     {
-        if (Physics2D.Raycast(transform.position, 
-                Vector2.down, 
-                1f, 
-                groundLayer))
-        {
-            return true;
-        }
-        return false;
-    }
+        // Usamos el collider que esté activo en ese momento
+        Collider2D currentCol = _isRolling ? rollingCollider : standingCollider;
+        
+        float extraHeight = 0.1f;
+        
+        // Lanzamos un BoxCast o Raycast desde el centro del collider activo
+        RaycastHit2D hit = Physics2D.BoxCast(
+            currentCol.bounds.center, 
+            currentCol.bounds.size, 
+            0f, 
+            Vector2.down, 
+            extraHeight, 
+            groundLayer
+        );
 
+        return hit.collider != null;
+    }
 }
