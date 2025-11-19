7b3f567c80f3018550d660e23a0bd7d23718b1f2
DiIvVL04 <133398085+DiIvVL04@users.noreply.github.com>
2025-11-19 12:17:03 -0600

    [UPDATE] implementacion de MenuScript funcional (#8)
    * [ADD] agrego mas parte del nivel 1 y corrigo los assests de los controlles
    
    * [ADD] se agrega sistema de interaccion con los objetos
    
    * [UPDATE] implementacion de MenuScript funcional
    
    ---------
    
    Co-authored-by: unknown <20223tn012@utez.edu.mx>

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    //Meliplay
+    [SerializeField] private string gameScene;
 
-    // Update is called once per frame
-    void Update()
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene(gameScene);
     }
 }
